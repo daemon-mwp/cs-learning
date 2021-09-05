a4261e7496bd75dd1ce1f91acd730eca89dc1bd0
daemon-mwp <15683653001@163.com>
2021-09-05 16:22:14 +0800

    init

---
+++ src/main/java/mwp202109/cs_learning/CsLearningApplication.java
@@ -0,0 +1,13 @@
+package mwp202109.cs_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mwp202109/cs_learning/CsLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package mwp202109.cs_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
