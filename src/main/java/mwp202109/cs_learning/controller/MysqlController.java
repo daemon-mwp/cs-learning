f947a80b5869ca3b5ea49d6f9d970cb006ec80c7
mwp <15683653001@163.com>
2021-09-13 21:22:14 +0800

    #comment [add]集成swagger@mwp

--- src/main/java/mwp202109/cs_learning/controller/MysqlController.java
+++ src/main/java/mwp202109/cs_learning/controller/MysqlController.java
@@ -2,6 +2,8 @@ package mwp202109.cs_learning.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import mwp202109.cs_learning.dao.domain.DO.UserDO;
 import mwp202109.cs_learning.dao.mapper.UserMapper;
@@ -12,16 +14,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @RestController
+@Api(tags = "mysql相关练习接口")
 @RequestMapping(("/mysql"))
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class MysqlController {
     private final UserMapper userMapper;
 
+    @ApiOperation(value = "获取用户信息")
     @GetMapping("/getUser")
-    public Object getUser(@RequestParam @NotNull Long pageNo, @RequestParam @NotNull Long pageSize){
+    public IPage<UserDO> getUser(@RequestParam @NotNull Long pageNo, @RequestParam @NotNull Long pageSize){
         return userMapper.getAllUser(new Page(pageNo, pageSize));
     }
 
--- src/main/java/mwp202109/cs_learning/dao/domain/DO/UserDO.java
+++ src/main/java/mwp202109/cs_learning/dao/domain/DO/UserDO.java
@@ -1,15 +1,22 @@
 package mwp202109.cs_learning.dao.domain.DO;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class UserDO {
+    @ApiModelProperty(value = "主键id")
     private Integer id;
+    @ApiModelProperty(value = "用户名")
     private String username;
+    @ApiModelProperty(value = "中文名")
     private String chineseName;
+    @ApiModelProperty(value = "密码")
     private String password;
+    @ApiModelProperty(value = "创建时间")
     private LocalDateTime createTime;
+    @ApiModelProperty(value = "更新时间")
     private LocalDateTime updateTime;
 }
